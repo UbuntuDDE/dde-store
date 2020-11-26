d384acfcd283d67c1d51aa276de9e67fa02b6bcd
Ahmad Afef <andy.afef@gmail.com>
2020-11-26 07:03:23 +0200

    dde store Arabic translation creation
    Just creating the Arabic file.

---
+++ translations/dde-store_ar.ts
@@ -0,0 +1,537 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ar" version="2.1">
+
+    <context>
+
+        <name>main</name>
+
+        <message>
+
+            <location filename="../main.cpp" line="17"/>
+
+            <source>An app store for DDE built with DTK</source>
+
+            <translation>An app store for DDE built with DTK</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../main.cpp" line="17"/>
+
+            <source>Created by %1</source>
+
+            <translation>Created by %1</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>MainWindow</name>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="91"/>
+
+            <source>Settings</source>
+
+            <translation>Settings</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="115"/>
+
+            <source>Home</source>
+
+            <translation>Home</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="116"/>
+
+            <source>Messaging</source>
+
+            <translation>Messaging</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="117"/>
+
+            <source>Internet</source>
+
+            <translation>Internet</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="118"/>
+
+            <source>Games</source>
+
+            <translation>Games</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="119"/>
+
+            <source>Development</source>
+
+            <translation>Development</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="120"/>
+
+            <source>Office</source>
+
+            <translation>Office</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="121"/>
+
+            <source>Graphics</source>
+
+            <translation>Graphics</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="122"/>
+
+            <source>Video</source>
+
+            <translation>Video</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="123"/>
+
+            <source>Music</source>
+
+            <translation>Music</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="124"/>
+
+            <source>System</source>
+
+            <translation>System</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="125"/>
+
+            <source>Installed</source>
+
+            <translation>Installed</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="230"/>
+
+            <source>Cannot close while app is being installed</source>
+
+            <translation>Cannot close while app is being installed</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../mainwindow.cpp" line="231"/>
+
+            <source>OK</source>
+
+            <translation>OK</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>CategoryPage</name>
+
+        <message>
+
+            <location filename="../pages/categorypage.cpp" line="15"/>
+
+            <source>Sort by</source>
+
+            <translation>Sort by</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/categorypage.cpp" line="18"/>
+
+            <source>Alphabetical</source>
+
+            <translation>Alphabetical</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/categorypage.cpp" line="19"/>
+
+            <source>Ratings</source>
+
+            <translation>Ratings</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/categorypage.cpp" line="39"/>
+
+            <source>No results for %1</source>
+
+            <translation>No results for %1</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>HomePage</name>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="50"/>
+
+            <source>Messaging</source>
+
+            <translation>Messaging</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="51"/>
+
+            <source>Internet</source>
+
+            <translation>Internet</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="52"/>
+
+            <source>Games</source>
+
+            <translation>Games</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="53"/>
+
+            <source>Development</source>
+
+            <translation>Development</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="54"/>
+
+            <source>Office</source>
+
+            <translation>Office</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="55"/>
+
+            <source>Graphics</source>
+
+            <translation>Graphics</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="56"/>
+
+            <source>Video</source>
+
+            <translation>Video</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="57"/>
+
+            <source>Music</source>
+
+            <translation>Music</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="58"/>
+
+            <source>System</source>
+
+            <translation>System</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/homepage.cpp" line="91"/>
+
+            <source>View More</source>
+
+            <translation>View More</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>ItemPage</name>
+
+        <message>
+
+            <location filename="../pages/itempage.cpp" line="62"/>
+
+            <source>Uninstall</source>
+
+            <translation>Uninstall</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/itempage.cpp" line="86"/>
+
+            <source>No description provided.</source>
+
+            <translation>No description provided.</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/itempage.cpp" line="101"/>
+
+            <source>Install (%1)</source>
+
+            <translation>Install (%1)</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/itempage.cpp" line="110"/>
+
+            <source>Open</source>
+
+            <translation>Open</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>UpdatesPage</name>
+
+        <message>
+
+            <location filename="../pages/updatespage.cpp" line="12"/>
+
+            <source>Updates</source>
+
+            <translation>Updates</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/updatespage.cpp" line="13"/>
+
+            <source>Up to date</source>
+
+            <translation>Up to date</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/updatespage.cpp" line="15"/>
+
+            <source>System Updates</source>
+
+            <translation>System Updates</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/updatespage.cpp" line="27"/>
+
+            <source>Update All</source>
+
+            <translation>Update All</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/updatespage.cpp" line="50"/>
+
+            <source>Updating...</source>
+
+            <translation>Updating...</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../pages/updatespage.cpp" line="53"/>
+
+            <source>Update All (%1)</source>
+
+            <translation>Update All (%1)</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>stars</name>
+
+        <message>
+
+            <location filename="../widgets/stars.cpp" line="23"/>
+
+            <source>(%1 ratings)</source>
+
+            <translation>(%1 ratings)</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>AppStreamHelper</name>
+
+        <message>
+
+            <location filename="../backend/appstreamhelper.cpp" line="28"/>
+
+            <source>ERROR: Unable to open AppStream metadata pool</source>
+
+            <translation>ERROR: Unable to open AppStream metadata pool</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../backend/appstreamhelper.cpp" line="32"/>
+
+            <source>OK</source>
+
+            <translation>OK</translation>
+
+        </message>
+
+    </context>
+
+    <context>
+
+        <name>PackageKitHelper</name>
+
+        <message>
+
+            <location filename="../backend/packagekithelper.cpp" line="59"/>
+
+            <source>Updates Available</source>
+
+            <translation>Updates Available</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../backend/packagekithelper.cpp" line="120"/>
+
+            <source>Installed \"%1\"</source>
+
+            <translation>Installed \"%1\"</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../backend/packagekithelper.cpp" line="140"/>
+
+            <source>Uninstalled \"%1\"</source>
+
+            <translation>Uninstalled \"%1\"</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../backend/packagekithelper.cpp" line="167"/>
+
+            <source>Updates Installed</source>
+
+            <translation>Updates Installed</translation>
+
+        </message>
+
+        <message>
+
+            <location filename="../backend/packagekithelper.cpp" line="176"/>
+
+            <source>PackageKit Error</source>
+
+            <translation>PackageKit Error</translation>
+
+        </message>
+
+    </context>
+
+</TS>
