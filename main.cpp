a68d326955d639af87324bd7603423b912e904d6
dekzi <dekzidev@protonmail.com>
2020-12-06 11:44:33 -0800

    Open window in center

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include <DApplication>
 #include <DAboutDialog>
+#include <DWidgetUtil>
 
 DWIDGET_USE_NAMESPACE
 
@@ -16,7 +17,8 @@ int main(int argc, char *argv[])
     about.setProductName("DDE Store");
     about.setDescription(qApp->translate("main", "An app store for DDE built with DTK") + "<br/>" + qApp->translate("main", "Created by %1").arg("<a href='https://github.com/dekzi'>Dekzi</a>") + "<br/><br/><a href='https://github.com/dekzi/dde-store'>https://github.com/dekzi/dde-store</a>");
     about.setVersion("1.0.3");
-    MainWindow w;
-    w.show();
+    MainWindow *w = new MainWindow;
+    Dtk::Widget::moveToCenter(w);
+    w->show();
     return a.exec();
 }
