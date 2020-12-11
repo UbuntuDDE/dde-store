5bc2f444c56f3224356b1c933c090805b0194d34
dekzi <dekzidev@protonmail.com>
2020-12-11 01:26:09 -0800

    Implement Deepin Turbo

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ DWIDGET_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
-    DApplication a(argc, argv);
-    a.loadTranslator();
-    a.setApplicationName("dde-store");
-    a.setOrganizationName("dekzi");
+    DApplication *app = new DApplication(argc, argv);
+    app->loadTranslator();
+    app->setApplicationName("dde-store");
+    app->setOrganizationName("dekzi");
     DAboutDialog about;
-    a.setAboutDialog(&about);
+    app->setAboutDialog(&about);
     about.setProductIcon(QIcon::fromTheme("deepin-app-store"));
     about.setProductName("DDE Store");
     about.setDescription(qApp->translate("main", "An app store for DDE built with DTK") + "<br/>" + qApp->translate("main", "Created by %1").arg("<a href='https://github.com/dekzi'>Dekzi</a>") + "<br/><br/><a href='https://github.com/dekzi/dde-store'>https://github.com/dekzi/dde-store</a>");
     about.setVersion("1.0.4");
-    MainWindow *w = new MainWindow;
-    Dtk::Widget::moveToCenter(w);
-    w->show();
-    return a.exec();
+    MainWindow *window = new MainWindow;
+    Dtk::Widget::moveToCenter(window);
+    window->show();
+    return app->exec();
 }
