f1023cc924df4e1a8fd929c37e8350bfbba35bfe
dekzi <dekzidev@protonmail.com>
2020-12-13 20:06:06 -0800

    1.1.0 Update

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     about.setProductIcon(QIcon::fromTheme("deepin-app-store"));
     about.setProductName("DDE Store");
     about.setDescription(qApp->translate("main", "An app store for DDE built with DTK") + "<br/>" + qApp->translate("main", "Created by %1").arg("<a href='https://github.com/dekzi'>Dekzi</a>") + "<br/><br/><a href='https://github.com/dekzi/dde-store'>https://github.com/dekzi/dde-store</a>");
-    about.setVersion("1.0.4");
+    about.setVersion("1.1.0");
     MainWindow *window = new MainWindow;
     Dtk::Widget::moveToCenter(window);
     window->show();
