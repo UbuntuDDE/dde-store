06f85fadd50522601e74ea35ad3772d5dc6538c6
水货 <1365143958@qq.com>
2021-01-06 12:15:44 +0800

    Add zh_CN

---
+++ translations/dde-store_zh_CN.ts
@@ -0,0 +1,435 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AppStreamHelper</name>
+    <message>
+        <location filename="../backend/appstreamhelper.cpp" line="30"/>
+        <source>ERROR: Unable to open AppStream metadata pool</source>
+        <translation>错误：无法打开 AppStream 元数据池</translation>
+    </message>
+    <message>
+        <location filename="../backend/appstreamhelper.cpp" line="34"/>
+        <source>OK</source>
+        <translation>哦</translation>
+    </message>
+</context>
+<context>
+    <name>CategoryPage</name>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="17"/>
+        <source>Sort by</source>
+        <translation>排序方式</translation>
+    </message>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="20"/>
+        <source>Alphabetical</source>
+        <translation>字母</translation>
+    </message>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="21"/>
+        <source>Ratings</source>
+        <translation type="unfinished">评级</translation>
+    </message>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="55"/>
+        <source>No results for %1</source>
+        <translation>没有找到 %1</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../pages/homepage.cpp" line="48"/>
+        <source>Messaging</source>
+        <translation>消息</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="49"/>
+        <source>Internet</source>
+        <translation>网络</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="50"/>
+        <source>Games</source>
+        <translation>游戏</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="51"/>
+        <source>Development</source>
+        <translation>开发</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="52"/>
+        <source>Office</source>
+        <translation>办公</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="53"/>
+        <source>Graphics</source>
+        <translation>图形图像</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="54"/>
+        <source>Video</source>
+        <translation>视频</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="55"/>
+        <source>Music</source>
+        <translation>音乐</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="56"/>
+        <source>System</source>
+        <translation>系统</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="90"/>
+        <source>View More</source>
+        <translation>查看更多</translation>
+    </message>
+</context>
+<context>
+    <name>ItemPage</name>
+    <message>
+        <location filename="../pages/itempage.cpp" line="78"/>
+        <source>Uninstall</source>
+        <translation>卸载</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="102"/>
+        <source>No description provided.</source>
+        <translation>没有提供描述。</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="119"/>
+        <source>Install (%1)</source>
+        <translation>安装（%1）</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="135"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="35"/>
+        <source>Check for updates</source>
+        <translation>检查更新</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="36"/>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="133"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="160"/>
+        <source>Home</source>
+        <translation>主页</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="161"/>
+        <source>Messaging</source>
+        <translation>消息</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="162"/>
+        <source>Internet</source>
+        <translation>网络</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="163"/>
+        <source>Games</source>
+        <translation>游戏</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="164"/>
+        <source>Development</source>
+        <translation>开发</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="165"/>
+        <source>Office</source>
+        <translation>办公</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="166"/>
+        <source>Graphics</source>
+        <translation>图形图像</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="167"/>
+        <source>Video</source>
+        <translation>视频</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="168"/>
+        <source>Music</source>
+        <translation>音乐</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="169"/>
+        <source>System</source>
+        <translation>系统</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="170"/>
+        <source>Installed</source>
+        <translation>已装</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="288"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="290"/>
+        <source>Minimize to system tray</source>
+        <translation>最小化到系统托盘</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="292"/>
+        <source>Do not ask again</source>
+        <translation>不再询问</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="294"/>
+        <source>Please choose your action</source>
+        <translation>请选择你的操作</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="299"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="300"/>
+        <source>Confirm</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="331"/>
+        <source>Cannot close while app is being installed</source>
+        <translation>不能在安装应用程序时关闭</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="332"/>
+        <source>OK</source>
+        <translation>知道了</translation>
+    </message>
+</context>
+<context>
+    <name>PackageKitHelper</name>
+    <message>
+        <location filename="../backend/packagekithelper.cpp" line="85"/>
+        <source>Updates Available</source>
+        <translation>更新就绪</translation>
+    </message>
+    <message>
+        <location filename="../backend/packagekithelper.cpp" line="147"/>
+        <source>Installed &quot;%1&quot;</source>
+        <translation>已安装 %1</translation>
+    </message>
+    <message>
+        <location filename="../backend/packagekithelper.cpp" line="170"/>
+        <source>Uninstalled &quot;%1&quot;</source>
+        <translation>已卸载 %1</translation>
+    </message>
+    <message>
+        <location filename="../backend/packagekithelper.cpp" line="193"/>
+        <source>Updates Installed</source>
+        <translation>已安装更新</translation>
+    </message>
+    <message>
+        <location filename="../backend/packagekithelper.cpp" line="202"/>
+        <source>PackageKit Error</source>
+        <translation>包工具出错</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../backend/settings.cpp" line="85"/>
+        <source>Basic</source>
+        <translation>基础设置</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="86"/>
+        <source>View</source>
+        <translation>视图</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="87"/>
+        <source>Behaviour</source>
+        <translation>行为</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="88"/>
+        <source>Notifications</source>
+        <translation>通知</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="89"/>
+        <source>Show non-app updates as</source>
+        <translation>没有软件更新时显示</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="90"/>
+        <source>Max apps per list</source>
+        <translation>列表最大软件数</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="91"/>
+        <source>Minimize to tray on exit</source>
+        <translation>退出时最小化到托盘</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="92"/>
+        <source>Check for updates</source>
+        <translation>检查更新频率</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="93"/>
+        <source>App installed</source>
+        <translation>安装完软件</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="94"/>
+        <source>App uninstalled</source>
+        <translation>卸载完软件</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="95"/>
+        <source>Available updates</source>
+        <translation>更新就绪</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="96"/>
+        <source>Finished updates</source>
+        <translation>更新完成</translation>
+    </message>
+</context>
+<context>
+    <name>SnapPlugin</name>
+    <message>
+        <location filename="../backend/snapplugin.cpp" line="112"/>
+        <source>Installed &quot;%1&quot;</source>
+        <translation>已安装 %1</translation>
+    </message>
+    <message>
+        <location filename="../backend/snapplugin.cpp" line="150"/>
+        <source>Uninstalled &quot;%1&quot;</source>
+        <translation>已卸载 %1</translation>
+    </message>
+    <message>
+        <location filename="../backend/snapplugin.cpp" line="218"/>
+        <source>Classic mode required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../backend/snapplugin.cpp" line="218"/>
+        <source>This snap requires confinement to be disabled via classic mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../backend/snapplugin.cpp" line="220"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../backend/snapplugin.cpp" line="221"/>
+        <source>Continue</source>
+        <translation>继续</translation>
+    </message>
+    <message>
+        <location filename="../backend/snapplugin.cpp" line="227"/>
+        <source>Snap Error</source>
+        <translation type="unfinished">Snap 出错</translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesPage</name>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="14"/>
+        <source>Updates</source>
+        <translation>更新</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="15"/>
+        <source>Up to date</source>
+        <translation>已是最新</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="17"/>
+        <source>System Updates</source>
+        <translation>系统更新</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="42"/>
+        <source>Update All</source>
+        <translation>全部更新</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="75"/>
+        <source>Updating...</source>
+        <translation>正在更新……</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="83"/>
+        <source>Update All (%1)</source>
+        <translation>全部更新（%1）</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="19"/>
+        <source>An app store for DDE built with DTK</source>
+        <translation>一个用了 DTK 给深度桌面环境开发的应用软件商店</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="19"/>
+        <source>Created by %1</source>
+        <translation>由 %1 进行开发</translation>
+    </message>
+</context>
+<context>
+    <name>settings</name>
+    <message>
+        <location filename="../backend/settings.cpp" line="28"/>
+        <source>&quot;System Updates&quot;</source>
+        <translation>系统更新</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="28"/>
+        <source>Individual Packages</source>
+        <translation>单独的包</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="29"/>
+        <source>Hourly</source>
+        <translation>每小时</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="29"/>
+        <source>Never</source>
+        <translation>不检查</translation>
+    </message>
+</context>
+<context>
+    <name>stars</name>
+    <message>
+        <location filename="../widgets/stars.cpp" line="23"/>
+        <source>(%1 ratings)</source>
+        <translation type="unfinished">（%1 个评级）</translation>
+    </message>
+</context>
+</TS>
