e7db90fe88d0234b8d91e6fa350ab3d603b28ba1
SelyanKab <selyan.kab@protonmail.com>
2021-05-27 17:23:39 +0100

    Update dde-store_kab.ts

--- translations/dde-store_kab.ts
+++ translations/dde-store_kab.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../pages/categorypage.cpp" line="58"/>
         <source>No results for %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ulac agemmuḍ i %1</translation>
     </message>
 </context>
 <context>
@@ -29,27 +29,27 @@
     <message>
         <location filename="../pages/homepage.cpp" line="46"/>
         <source>Messaging</source>
-        <translation type="unfinished"></translation>
+        <translation>Izen</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="47"/>
         <source>Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Internet</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="48"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>Uraren</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="49"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Taneflit</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="50"/>
         <source>Office</source>
-        <translation type="unfinished"></translation>
+        <translation>Tanarit</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="51"/>
@@ -59,22 +59,22 @@
     <message>
         <location filename="../pages/homepage.cpp" line="52"/>
         <source>Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Ividyuten</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="53"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Aẓawan</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="54"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Anagraw</translation>
     </message>
     <message>
         <location filename="../pages/homepage.cpp" line="89"/>
         <source>View More</source>
-        <translation type="unfinished"></translation>
+        <translation>Sken-d ugar</translation>
     </message>
 </context>
 <context>
@@ -82,7 +82,7 @@
     <message>
         <location filename="../pages/itempage.cpp" line="76"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Kkes</translation>
     </message>
     <message>
         <location filename="../pages/itempage.cpp" line="109"/>
@@ -102,12 +102,12 @@
     <message>
         <location filename="../pages/itempage.cpp" line="145"/>
         <source>Install (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation>Sebded l (%1)</translation>
     </message>
     <message>
         <location filename="../pages/itempage.cpp" line="151"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Ldi</translation>
     </message>
 </context>
 <context>
@@ -115,47 +115,47 @@
     <message>
         <location filename="../mainwindow.cpp" line="38"/>
         <source>Check for updates</source>
-        <translation type="unfinished">Check for updates</translation>
+        <translation>Senqed ileqman</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="39"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Ffeɣ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="136"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Iɣewwaṛen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="164"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Agejdan</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="165"/>
         <source>Messaging</source>
-        <translation type="unfinished"></translation>
+        <translation>Izen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="166"/>
         <source>Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Internet</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="167"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>Uraren</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="168"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Taneflit</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="169"/>
         <source>Office</source>
-        <translation type="unfinished"></translation>
+        <translation>Tanarit</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="170"/>
@@ -165,32 +165,32 @@
     <message>
         <location filename="../mainwindow.cpp" line="171"/>
         <source>Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Ividyuten</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="172"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Aẓawan</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="173"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Anagraw</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="174"/>
         <source>Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Ibdded</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="175"/>
         <source>Updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Aleqqem</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="243"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Ffeɣ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="245"/>
@@ -200,22 +200,22 @@
     <message>
         <location filename="../mainwindow.cpp" line="247"/>
         <source>Do not ask again</source>
-        <translation type="unfinished"></translation>
+        <translation>Ur d-sutur ara tikkelt-nniḍen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="249"/>
         <source>Please choose your action</source>
-        <translation type="unfinished"></translation>
+        <translation>Ttxil fren tigawt</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="254"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Sefsex</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="255"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>Sentem</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="285"/>
@@ -225,7 +225,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="286"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>IH</translation>
     </message>
 </context>
 <context>
