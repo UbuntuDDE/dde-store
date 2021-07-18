1f8dc4c40943dc09e96432c37d45409d3e6714dd
thellage <45451453+thellage@users.noreply.github.com>
2021-07-18 18:11:09 +1200

    Add files via upload

---
+++ translations/dde-store_ru.ts
@@ -0,0 +1,412 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>CategoryPage</name>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="12"/>
+        <source>Sort by</source>
+        <translation>Сортировать</translation>
+    </message>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="15"/>
+        <source>Alphabetical</source>
+        <translation>По алфавиту</translation>
+    </message>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="16"/>
+        <source>Ratings</source>
+        <translation>По оценке</translation>
+    </message>
+    <message>
+        <location filename="../pages/categorypage.cpp" line="58"/>
+        <source>No results for %1</source>
+        <translation>Нет результатов для %1</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <location filename="../pages/homepage.cpp" line="46"/>
+        <source>Messaging</source>
+        <translation>Обмен сообщениями</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="47"/>
+        <source>Internet</source>
+        <translation>Интернет</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="48"/>
+        <source>Games</source>
+        <translation>Игры</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="49"/>
+        <source>Development</source>
+        <translation>Разработка</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="50"/>
+        <source>Office</source>
+        <translation>Офис</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="51"/>
+        <source>Graphics</source>
+        <translation>Графика</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="52"/>
+        <source>Video</source>
+        <translation>Видео</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="53"/>
+        <source>Music</source>
+        <translation>Музыка</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="54"/>
+        <source>System</source>
+        <translation>Система</translation>
+    </message>
+    <message>
+        <location filename="../pages/homepage.cpp" line="89"/>
+        <source>View More</source>
+        <translation>Показать больше</translation>
+    </message>
+</context>
+<context>
+    <name>ItemPage</name>
+    <message>
+        <location filename="../pages/itempage.cpp" line="76"/>
+        <source>Uninstall</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="109"/>
+        <source>No description provided.</source>
+        <translation>Описание отсуствует.</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="126"/>
+        <source>Installed &quot;%1&quot;</source>
+        <translation>&quot;%1&quot; установлен</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="130"/>
+        <source>Uninstalled &quot;%1&quot;</source>
+        <translation>&quot;%1&quot; удален</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="145"/>
+        <source>Install (%1)</source>
+        <translation>Установить (%1)</translation>
+    </message>
+    <message>
+        <location filename="../pages/itempage.cpp" line="151"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="38"/>
+        <source>Check for updates</source>
+        <translation>Проверить обновления</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>Quit</source>
+        <translation>Выйти</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="136"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="164"/>
+        <source>Home</source>
+        <translation>На домашнюю страницу</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="165"/>
+        <source>Messaging</source>
+        <translation>Обмен сообщениями</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="166"/>
+        <source>Internet</source>
+        <translation>Интернет</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="167"/>
+        <source>Games</source>
+        <translation>Игры</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="168"/>
+        <source>Development</source>
+        <translation>Разработка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="169"/>
+        <source>Office</source>
+        <translation>Офис</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="170"/>
+        <source>Graphics</source>
+        <translation>Графика</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="171"/>
+        <source>Video</source>
+        <translation>Видео</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="172"/>
+        <source>Music</source>
+        <translation>Музыка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="173"/>
+        <source>System</source>
+        <translation>Система</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="174"/>
+        <source>Installed</source>
+        <translation>Установленное</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="175"/>
+        <source>Updates</source>
+        <translation>Обновления</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="243"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="245"/>
+        <source>Minimize to system tray</source>
+        <translation>Свернуть в трей</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>Do not ask again</source>
+        <translation>Больше не спрашивать</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="249"/>
+        <source>Please choose your action</source>
+        <translation>Пожалуйста, выберите действие</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="254"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="255"/>
+        <source>Confirm</source>
+        <translation>Подтвердить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="285"/>
+        <source>Cannot close while app is being installed</source>
+        <translation>Невозможно закрыть окно, пока приложение не закончило установку</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="286"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+</context>
+<context>
+    <name>PackageKitSource</name>
+    <message>
+        <location filename="../backend/sources/packagekit/packagekitsource.cpp" line="16"/>
+        <source>ERROR: Unable to open AppStream metadata pool</source>
+        <translation>ОШИБКА: Невозможно открыть пул метаданных AppStream</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../backend/settings.cpp" line="85"/>
+        <source>Basic</source>
+        <translation>Базовый</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="86"/>
+        <source>View</source>
+        <translation>Отобразить</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="87"/>
+        <source>Behaviour</source>
+        <translation>Поведение</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="88"/>
+        <source>Notifications</source>
+        <translation>Уведомления</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="89"/>
+        <source>Show non-app updates as</source>
+        <translation>Показать обновления, не касающиеся приложений, как</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="90"/>
+        <source>Max apps per list</source>
+        <translation>Максимум приложений в списке</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="91"/>
+        <source>Minimize to tray on exit</source>
+        <translation>Свернуть в трей при выходе</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="92"/>
+        <source>Check for updates</source>
+        <translation>Проверить обновления</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="93"/>
+        <source>App installed</source>
+        <translation>Приложение установлено</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="94"/>
+        <source>App uninstalled</source>
+        <translation>Приложение удалено</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="95"/>
+        <source>Available updates</source>
+        <translation>Доступные обновления</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="96"/>
+        <source>Finished updates</source>
+        <translation>Завершенные обновления</translation>
+    </message>
+</context>
+<context>
+    <name>SnapSource</name>
+    <message>
+        <location filename="../backend/sources/snap/snapsource.cpp" line="202"/>
+        <source>Classic mode required</source>
+        <translation>Требуется классический режим</translation>
+    </message>
+    <message>
+        <location filename="../backend/sources/snap/snapsource.cpp" line="202"/>
+        <source>This snap requires confinement to be disabled via classic mode</source>
+        <translation>Этот Snap пакет требует подтверждения для отключения через классический режим</translation>
+    </message>
+    <message>
+        <location filename="../backend/sources/snap/snapsource.cpp" line="204"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../backend/sources/snap/snapsource.cpp" line="205"/>
+        <source>Continue</source>
+        <translation>Продолжить</translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesPage</name>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="13"/>
+        <source>Updates</source>
+        <translation>Обновления</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="14"/>
+        <source>Up to date</source>
+        <translation>Обновления отсутствуют</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="16"/>
+        <source>System Updates</source>
+        <translation>Системные обновления</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="50"/>
+        <source>Update All</source>
+        <translation>Обновить все</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="54"/>
+        <source>Updating...</source>
+        <translation>Идет обновление...</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="84"/>
+        <source>Update All (%1)</source>
+        <translation>Обновить все(%1)</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="86"/>
+        <source>Updates Available</source>
+        <translation>Доступные обновления</translation>
+    </message>
+    <message>
+        <location filename="../pages/updatespage.cpp" line="96"/>
+        <source>Updates Installed</source>
+        <translation>Установленные обновления</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="31"/>
+        <source>An app store for DDE built with DTK</source>
+        <translation>Магазин приложений для DDE, написанный с использованием DTK</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="31"/>
+        <source>Created by %1</source>
+        <translation>Разработан %1</translation>
+    </message>
+</context>
+<context>
+    <name>settings</name>
+    <message>
+        <location filename="../backend/settings.cpp" line="28"/>
+        <source>&quot;System Updates&quot;</source>
+        <translation>&quot;Обновления системы&quot;</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="28"/>
+        <source>Individual Packages</source>
+        <translation>Отдельные пакеты</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="29"/>
+        <source>Hourly</source>
+        <translation>Каждый час</translation>
+    </message>
+    <message>
+        <location filename="../backend/settings.cpp" line="29"/>
+        <source>Never</source>
+        <translation>Никогда</translation>
+    </message>
+</context>
+<context>
+    <name>stars</name>
+    <message>
+        <location filename="../widgets/stars.cpp" line="23"/>
+        <source>(%1 ratings)</source>
+        <translation>(Оценки %1)</translation>
+    </message>
+</context>
+</TS>
